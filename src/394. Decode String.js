5fe00a258a442539a017ff5d5a2b3f49c1ed8911
zhoususu <431185521@qq.com>
2020-05-29 02:32:08 +0800

    add: 394

--- src/394. Decode String.js
+++ src/394. Decode String.js
@@ -21,6 +21,22 @@ s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
 /**
  * @param {string} s
  * @return {string}
+ * 
+ * 
+ * 这个题解抄@krahets 的
+ * 
+ * 整理思路： 需要处理：1.多组嵌套encode string，所以用栈保存上一组string的状态（也可以用递归） 2.多位数字 
+ *   将整个encode string当成：lastString + currentString * count，完事
+ *   stack保存count和lastString。
+ *   1.遇到左括号的时候入栈，push lastString以及currentString的count
+ *    * 如果遇到嵌套左括号，如3[asd2[cvb3[dfg]]]，stack就像['',3,'asd',2,'cvb',3]
+ *   2.遇到右括号的时候出栈，pop lastString和currentString的count，结合currentString，构造ret，这个ret将作为下一次入栈的lastString
+ *   3.遇到字符，将字符插到ret末尾，把ret当成currentString和lastString
+ *   4.遇到数字，则构造count
+ *
+ * 坑：
+ *  自己刚开始写的时候遇到数字就入栈...，需要在遇到左中括号的时候再进行入栈操作，不能遇到数字就入栈，数字不止一位数
+ *  
  */
 var decodeString = function(s) {
   let ret = '', stk = [], count = '';
